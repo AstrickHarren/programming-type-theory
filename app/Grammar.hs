cb29a6ec071bab0019bcfc3ca45f77da222674ba
Astrick <astrick.harren.at.work@gmail.com>
2022-06-01 14:10:01 +0000

    rf: separate out `show` from grammar

--- app/Grammar.hs
+++ app/Grammar.hs
@@ -1,6 +1,6 @@
 module Grammar where
 
-import Data.Set (Set, fromList)
+import Data.Set (Set, fromList, member)
 
 type Var = String
 
@@ -29,38 +29,3 @@ alphaBet = fromList $ map (: []) ['a' .. 'z']
 
 -- Context
 type Context = [TypedVar]
-
--- Show
-
-instance Show TypedVar where
-  show (TypedVar x ϕ) = show x ++ ":" ++ show ϕ
-
-instance Show Type where
-  show (Type s) = s
-  show (SuchThat ϕ m) = show ϕ ++ show m
-  show (Forall x ϕ) = "∀" ++ show x ++ "." ++ show ϕ
-  show (And ϕ ψ) = show ϕ ++ "∧" ++ show ψ
-  show (Or ϕ ψ) = show ϕ ++ "∨" ++ show ψ
-
-instance Show Term where
-  show (Term s) = s
-  show (Lambda x m) = "λ" ++ show x ++ "." ++ show m
-  show (Applied m n) =
-    "(" ++ show m ++ ")"
-      ++ if length (show n) > 1 then "(" ++ show n ++ ")" else show n
-  show (Pair m n) = "⟨" ++ show m ++ ", " ++ show n ++ "⟩"
-  show (OneOf ϕ ψ m) = case m of
-    Left n -> show n
-    Right n -> show n
-  show (Case l (x, m) (y, n)) =
-    "case ("
-      ++ show l
-      ++ "; "
-      ++ show x
-      ++ "."
-      ++ show m
-      ++ "; "
-      ++ show y
-      ++ "."
-      ++ show n
-      ++ ")"
---
+++ app/Show.hs
@@ -0,0 +1,41 @@
+module Show where
+
+import Data.Set (member)
+import FreeVar (FV (fv))
+import Grammar (Term (..), Type (..), TypedVar (..))
+
+instance Show TypedVar where
+  show (TypedVar x ϕ) = show x ++ ":" ++ show ϕ
+
+instance Show Type where
+  show (Type s) = s
+  show (SuchThat ϕ m) = show ϕ ++ show m
+  show (Forall x@(TypedVar v σ) ϕ) =
+    if v `member` fv ϕ
+      then "∀" ++ show x ++ "." ++ show ϕ
+      else show σ ++ " -> " ++ show ϕ
+  show (And ϕ ψ) = show ϕ ++ "∧" ++ show ψ
+  show (Or ϕ ψ) = show ϕ ++ "∨" ++ show ψ
+
+instance Show Term where
+  show (Term s) = s
+  show (Lambda x m) = "λ" ++ show x ++ "." ++ show m
+  show (Applied m n) =
+    "(" ++ show m ++ ")"
+      ++ if length (show n) > 1 then "(" ++ show n ++ ")" else show n
+  show (Pair m n) = "⟨" ++ show m ++ ", " ++ show n ++ "⟩"
+  show (OneOf ϕ ψ m) = case m of
+    Left n -> show n
+    Right n -> show n
+  show (Case l (x, m) (y, n)) =
+    "case ("
+      ++ show l
+      ++ "; "
+      ++ show x
+      ++ "."
+      ++ show m
+      ++ "; "
+      ++ show y
+      ++ "."
+      ++ show n
+      ++ ")"
