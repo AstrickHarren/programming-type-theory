9f28ed5d53945a97b533cb7ecdc012fca2e4048e
Astrick <astrick.harren.at.work@gmail.com>
2022-06-02 00:36:49 +0000

    fix: wrong subsub not subing with orig subvar

--- app/Substitute.hs
+++ app/Substitute.hs
@@ -11,20 +11,21 @@ varNotIn = elemAt 0 . difference alphaBet
 class Substitute a where
   sub :: Var -> Term -> a -> a
 
-subsub :: Substitute a => Var -> Var -> Term -> a -> a
-subsub x z n m = sub z n (sub x (Term z) m)
+-- M[z/x][N/y]
+subsub :: Substitute a => Var -> Var -> Var -> Term -> a -> a
+subsub x z y n m = sub y n (sub x (Term z) m)
 
 instance Substitute Term where
   -- x[N/x] = N; x[N/y] = x;
   sub y n t@(Term x) = if x == y then n else t
   -- (λx:τ.M)[N/x] = (λx:τ.M);
-  -- (λx:τ.M)[N/y] = (λz:τ[N/z].M[z/x][N/z]) w/ z ∉ FV(N) ∪ FV(M) ∪ FV(τ) if x ∈ FV(N)
+  -- (λx:τ.M)[N/y] = (λz:τ[z/x][N/y].M[z/x][N/y]) w/ z ∉ FV(N) ∪ FV(M) ∪ FV(τ) if x ∈ FV(N)
   -- (λx:τ.M)[N/y] = (λx:τ[N/y].M[N/y]) if x ∉ FV(N)
   sub y n t@(Lambda (TypedVar x τ) m)
     | x == y = t
     | x `member` fv n =
       let z = varNotIn (fv t `union` fv n)
-       in Lambda (z `TypedVar` subsub x z n τ) (subsub x z n m)
+       in Lambda (z `TypedVar` subsub x z y n τ) (subsub x z y n m)
     | otherwise = Lambda (x `TypedVar` τ) (sub y n m)
   -- (PQ)[N/y] = (P[N/y])(Q[N/y])
   sub y n (Applied p q) = Applied (sub y n p) (sub y n q)
@@ -36,7 +37,7 @@ instance Substitute Term where
     Right m -> Right $ sub y n m
   -- case(L;x.P;y.Q)[N/z] = case(L[N/z];(x.P)[N/z];(y.Q)[N/z])
   -- (x.M)[N/x] = (x.M)
-  -- (x.M)[N/y] = (z.M[z/x][N/z]) w/ z ∉ FV(N) ∪ FV(M) if x ∈ FV(N)
+  -- (x.M)[N/y] = (z.M[z/x][N/y]) w/ z ∉ FV(N) ∪ FV(M) if x ∈ FV(N)
   -- (x.M)[N/y] = (x.M[N/y]) if x ∉ FV(N)
   sub z n (Case l (x, p) (y, q)) =
     Case
@@ -48,7 +49,7 @@ instance Substitute Term where
         | x == y = t
         | x `member` fv n =
           let z = varNotIn (fv n `union` fv m)
-           in (x, subsub x z n m)
+           in (x, subsub x z y n m)
         | otherwise = (x, sub y n m)
 
 instance Substitute Type where
@@ -57,13 +58,13 @@ instance Substitute Type where
   -- (ϕM)[N/x] = ϕ[N/x]M[N/x]
   sub x n (SuchThat ϕ m) = SuchThat (sub x n ϕ) (sub x n m)
   -- (∀x:ϕ.ψ)[N/x] = (∀x:ϕ.ψ)[N/x]
-  -- (∀x:ϕ.ψ)[N/y] = (∀z:ϕ[z/x][N/z].ψ[z/x][N/z]) w/ z ∉ FV(ϕ) ∪ FV(ψ) ∪ FV(N) if x ∈ FV(N)
+  -- (∀x:ϕ.ψ)[N/y] = (∀z:ϕ[z/x][N/y].ψ[z/x][N/y]) w/ z ∉ FV(ϕ) ∪ FV(ψ) ∪ FV(N) if x ∈ FV(N)
   -- (∀x:ϕ.ψ)[N/y] = (∀x:ϕ[N/y].ψ[N/y]) o.w.
   sub y n t@(Forall (TypedVar x ϕ) ψ)
     | x == y = t
     | x `member` fv n =
       let z = varNotIn (fv t `union` fv n)
-       in Forall (z `TypedVar` subsub x z n ϕ) (subsub x z n ψ)
+       in Forall (z `TypedVar` subsub x z y n ϕ) (subsub x z y n ψ)
     | otherwise = Forall (x `TypedVar` sub y n ϕ) (sub y n ψ)
   -- (ϕ ∧ ψ)[N/x] = (ϕ[N/x] ∧ ψ[N/x])
   sub x n (And ϕ ψ) = And (sub x n ϕ) (sub x n ψ)
