1914ab8e7c245434fcc5c4f827a36ff1ae92446e
Astrick <astrick.harren.at.work@gmail.com>
2022-06-02 04:23:31 +0000

    feat(util): multivariate for all

--- app/Util.hs
+++ app/Util.hs
@@ -4,3 +4,6 @@ import Grammar
 
 binaryType :: String -> Term -> Term -> Type
 binaryType p x = SuchThat (SuchThat (Type p) x)
+
+forall :: [TypedVar] -> Type -> Type
+forall xs ϕ = foldr Forall ϕ xs
