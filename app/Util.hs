097c03665a288b91b870cdcae82e1b06ee18f139
Astrick <astrick.harren.at.work@gmail.com>
2022-06-02 04:28:10 +0000

    feat(util): implication

--- app/Util.hs
+++ app/Util.hs
@@ -1,9 +1,20 @@
 module Util where
 
+import Data.Set (union)
+import FreeVar (FV (fv))
 import Grammar
+import Substitute (varNotIn)
 
 binaryType :: String -> Term -> Term -> Type
 binaryType p x = SuchThat (SuchThat (Type p) x)
 
 forall :: [TypedVar] -> Type -> Type
 forall xs ϕ = foldr Forall ϕ xs
+
+implies :: Type -> Type -> Type
+implies a b =
+  let z = varNotIn (fv a `union` fv b)
+   in Forall (z `TypedVar` a) b
+
+iff :: Type -> Type -> Type
+iff a b = And (a `implies` b) (b `implies` a)
