af3708ecf5f45a835a8d4ddb39fc2891b2802705
Astrick <astrick.harren.at.work@gmail.com>
2022-06-03 00:31:58 +0000

    feat(set): set theoretic equality defs

--- app/SetTheory.hs
+++ app/SetTheory.hs
@@ -1,1 +1,51 @@
 module SetTheory where
+
+import Grammar
+import TypeCheck
+import Util hiding (forall)
+import qualified Util
+
+--- Definitions
+set = flip TypedVar $ Type "S"
+
+empty = Term "∅"
+
+equals a b = binaryType "=" (Term a) (Term b)
+
+memberOf a b = binaryType "∈" (Term a) (Term b)
+
+subsetOf a b = binaryType "⊂" (Term a) (Term b)
+
+infixl 8 ∈
+
+(∈) = memberOf
+
+infixl 8 ⊂
+
+(⊂) = subsetOf
+
+infixl 6 ===
+
+(===) = equals
+
+-- Utils
+
+-- overload `for all` as `for all sets`
+forall :: [Var] -> Type -> Type
+forall = Util.forall . map set
+
+-- Axiomatic Defs
+
+equalDef =
+  forall ["A", "B"] $
+    "A" === "B"
+      --> forall
+        ["x"]
+        ((("x" ∈ "A") --> ("x" ∈ "B")) `And` (("x" ∈ "B") --> ("x" ∈ "A")))
+
+equalItrp =
+  forall ["A", "B"] $
+    "A" === "B"
+      <-- forall
+        ["x"]
+        ((("x" ∈ "A") --> ("x" ∈ "B")) `And` (("x" ∈ "B") --> ("x" ∈ "A")))
