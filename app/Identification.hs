257c6671e3dcf3e46a5940a34966eeefcfcf66a3
Astrick <astrick.harren.at.work@gmail.com>
2022-06-04 04:07:12 +0000

    rf: identify terms w/ equiv bounded vars

--- app/Identification.hs
+++ app/Identification.hs
@@ -3,27 +3,26 @@ module Identification where
 import Data.Set (member)
 import FreeVar
 import Grammar
+import Substitute (Substitute (sub))
 
 instance Eq TypedVar where
-  (TypedVar x ϕ) == (TypedVar y ψ) = (x == y) && (ϕ == ψ)
+  (TypedVar x ϕ) == (TypedVar y ψ) = x == y && ϕ == ψ
 
 instance Eq Type where
   (Type a) == (Type b) = a == b
-  (SuchThat ϕ m) == (SuchThat ψ n) = (ϕ == ψ) && (m == n)
-  (Forall (TypedVar x σ) ϕ) == (Forall (TypedVar y τ) ψ)
-    | not (x `member` fv ϕ) && not (y `member` fv ψ) = (σ == τ) && (ϕ == ψ)
-    | otherwise = (x == y) && (σ == τ) && (ϕ == ψ)
-  (And ϕ ψ) == (And α β) = (ϕ == α) && (ψ == β)
-  (Or ϕ ψ) == (Or α β) = (ϕ == α) && (ψ == β)
+  (SuchThat ϕ m) == (SuchThat ψ n) = ϕ == ψ && m == n
+  (Forall (TypedVar x σ) ϕ) == (Forall (TypedVar y τ) ψ) = sub x (Term y) ϕ == ψ
+  (And ϕ ψ) == (And α β) = ϕ == α && ψ == β
+  (Or ϕ ψ) == (Or α β) = ϕ == α && ψ == β
   _ == _ = False
 
 instance Eq Term where
   (Term x) == (Term y) = x == y
-  (Lambda x m) == (Lambda y n) = (x == y) && (m == n)
-  (Applied m n) == (Applied p q) = (m == p) && (n == q)
-  (Pair m n) == (Pair p q) = (m == p) && (n == q)
-  (OneOf a b c) == (OneOf d e f) = (a == d) && (b == e) && (c == f)
+  (Lambda (TypedVar x σ) m) == (Lambda (TypedVar y τ) n) = sub x (Term y) m == n
+  (Applied m n) == (Applied p q) = m == p && n == q
+  (Pair m n) == (Pair p q) = m == p && n == q
+  (OneOf a b c) == (OneOf d e f) = a == d && b == e && c == f
   (Fst a) == (Fst b) = a == b
   (Snd a) == (Snd b) = a == b
-  (Case a b c) == (Case d e f) = (a == d) && (b == e) && (c == f)
+  (Case a (x, α) (y, β)) == (Case d (v, ϕ) (w, ψ)) = a == d && sub x (Term v) α == ϕ && sub y (Term w) β == ψ
   _ == _ = False
